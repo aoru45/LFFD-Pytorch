efc29941de24ec208b8461ec9de301b58fb97b97
aoru45 <34237646+aoru45@users.noreply.github.com>
2020-04-02 15:41:25 +0800

    Update basket_utils.py

--- basket_utils.py
+++ basket_utils.py
@@ -4,32 +4,31 @@
 @Author: Aoru Xue
 @Date: 2019-09-02 01:32:25
 @LastEditors: Aoru Xue
-@LastEditTime: 2019-10-01 10:36:00
+@LastEditTime: 2019-10-02 18:37:41
 '''
 import torch
 import math
+from config import *
 
-
-def convert_locations_to_boxes(locations, center_form_priors, variance = 2):
+def convert_locations_to_boxes(locations, priors, variance = 2):
 
     # priors can have one dimension less.
-    if center_form_priors.dim() + 1 == locations.dim():
-        center_form_priors = center_form_priors.unsqueeze(0)
+    if priors.dim() + 1 == locations.dim():
+        priors = priors.unsqueeze(0)
     return torch.cat([
-        center_form_priors[..., :2] - locations[..., :2] * variance * center_form_priors[..., 2:],
-        center_form_priors[..., :2] - locations[..., 2:] * variance * center_form_priors[..., 2:]
+        priors[..., :2] - locations[..., :2]  * priors[..., 2:],
+        priors[..., :2] - locations[..., 2:]  * priors[..., 2:]
     ], dim=locations.dim() - 1)
 
 
-def convert_boxes_to_locations(corner_form_boxes, center_form_priors,variance = 2):
+def convert_boxes_to_locations(corner_form_boxes, priors,variance = 2):
     
-    if center_form_priors.dim() + 1 == corner_form_boxes.dim():
-        center_form_priors = center_form_priors.unsqueeze(0)
+    if priors.dim() + 1 == corner_form_boxes.dim():
+        priors = priors.unsqueeze(0)
     return torch.cat([
-        (center_form_priors[..., :2] - corner_form_boxes[..., :2]) / center_form_priors[..., 2:] / variance,
-        (center_form_priors[..., :2] - corner_form_boxes[..., 2:]) / center_form_priors[..., 2:] / variance
+        (priors[..., :2] - corner_form_boxes[..., :2]) / priors[..., 2:],
+        (priors[..., :2] - corner_form_boxes[..., 2:]) / priors[..., 2:]
     ], dim=corner_form_boxes.dim() - 1)
-
 
 def area_of(left_top, right_bottom):
     hw = torch.clamp(right_bottom - left_top, min=0.0)
@@ -49,57 +48,39 @@ def in_center(priors,gt):
     # 1,n,4    m,1,4
     s1 = (priors[...,0] > gt[...,0]) & (priors[...,1] > gt[...,1]) & (priors[...,0] < gt[...,2]) & (priors[...,1] < gt[...,3]) 
     return s1
-def assign_priors(gt_boxes, gt_labels, center_form_priors,
+def assign_priors(gt_boxes, gt_labels, priors,
                   iou_threshold):
     
     #print(gt_boxes,corner_form_priors)
     # size: num_priors x num_targets
-    s1 = in_center(center_form_priors.unsqueeze(1),gt_boxes.unsqueeze(0)) # 直接匹配在中间的
+    # prior 包含 gt
+    s1 = in_center(priors.unsqueeze(1),gt_boxes.unsqueeze(0)) # 直接匹配在中间的
 
-    s1[torch.sum(s1,dim = 1) > 1] = False # 同时有多个匹配的
+    #s1[torch.sum(s1,dim = 1) > 1] = False # 同时有多个匹配的
+    not_ignored = torch.ones(s1.size(0),dtype = torch.uint8)
+    not_ignored[torch.sum(s1,dim = 1) > 1] = 0
     best_target_per_prior, best_target_per_prior_index = s1.max(1)
     
     
-    labels = gt_labels[best_target_per_prior_index]
-    labels[best_target_per_prior == False] = 0  # the backgournd id
+    labels = gt_labels[best_target_per_prior_index] # (num_priors,1)
+    labels[best_target_per_prior == False] = 0  # the backgournd id,没有匹配的给背景
     
-    boxes = gt_boxes[best_target_per_prior_index]
+    boxes = gt_boxes[best_target_per_prior_index] #num_priors，４
     t = 0 
     center_form_gt_boxes = corner_form_to_center_form(boxes)
-    for f,scale in zip([127,127,63,63,31,15,15,15],[(10,15),(15,20),(20,40),(40,70),(70,110),(110,250),(250,400),(400,560)]):
-        # s2 = (center_form_gt_boxes[t:t+f*f,2] > scale[0]/512. * 0.9) & \
-        # (center_form_gt_boxes[t:t+f*f,2] < scale[0]/512.) & \
-        # (center_form_gt_boxes[t:t+f*f,2] > scale[1]/512.) & \
-        # (center_form_gt_boxes[t:t+f*f,2] < scale[1]/512. * 1.1)
+    for f,scale in zip(feature_maps,scales):
+        d = torch.min(center_form_gt_boxes[t:t+f*f, 2],center_form_gt_boxes[t:t+f*f, 3])
+        condition = (d < scale[0]/image_size) | (d > scale[1]/image_size) 
+        labels[t:t+f*f][condition] = 0 
         
-        # s3 = (center_form_gt_boxes[t:t+f*f,3] > scale[0]/512. * 0.9) & \
-        # (center_form_gt_boxes[t:t+f*f,3] < scale[0]/512.) & \
-        # (center_form_gt_boxes[t:t+f*f,3] > scale[1]/512.) & \
-        # (center_form_gt_boxes[t:t+f*f,3] < scale[1]/512. * 1.1)
-        s2 = (center_form_gt_boxes[t:t+f*f, 2] < scale[0]/512.) | \
-        (center_form_gt_boxes[t:t+f*f, 2] > scale[1]/512.) 
+        left_gray_scale = [0.9 *scale[0], scale[0]]
+        right_gray_scale = [scale[1], 1.1 * scale[1]]
+        
+        not_ignored[t:t+f*f][(d > left_gray_scale[0]) & (d < left_gray_scale[1])] = 0
+        not_ignored[t:t+f*f][(d > right_gray_scale[0]) & (d < right_gray_scale[1])] = 0
 
-        s3 = (center_form_gt_boxes[t:t+f*f, 3] < scale[0]/512.) | \
-           (center_form_gt_boxes[t:t+f*f, 3] > scale[1]/512.)
-        labels[t:t+f*f][s2 & s3] = 0 
-        # labels[t:t+f*f][s3==True] = 0
-        #labels[t:t+f*f][s4==True] = 0 
         t += f*f
-    
-    
-    # # size: num_priors
-    # best_target_per_prior, best_target_per_prior_index = ious.max(1)
-    # # size: num_targets
-    # best_prior_per_target, best_prior_per_target_index = ious.max(0)
-
-    # for target_index, prior_index in enumerate(best_prior_per_target_index):
-    #     best_target_per_prior_index[prior_index] = target_index
-    # best_target_per_prior.index_fill_(0, best_prior_per_target_index, 2)
-    # # size: num_priors
-    # labels = gt_labels[best_target_per_prior_index]
-    # labels[best_target_per_prior < iou_threshold] = 0  # the backgournd id
-    # boxes = gt_boxes[best_target_per_prior_index]
-    return boxes, labels
+    return boxes, labels, not_ignored
 
 
 def hard_negative_mining(loss, labels, neg_pos_ratio):
